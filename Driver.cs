c5cd2fed1dc62c7ff37ec379766509cc63b04022
coffeemlk8610 <50548932+coffeemlk8610@users.noreply.github.com>
2020-02-08 20:00:34 +0900

    あげてなかった分の追加

---
+++ Driver.cs
@@ -0,0 +1,26 @@
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.Bell
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                // Try initial values
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+                foreach (Result initial in initials)
+                {
+                    // "BellTest"関数を実行し、結果を出力しますvar res = BellTest.Run(sim, 1000, initial).Result;
+                    var (numZeros, numOnes) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                }
+            }
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+    }
+}
